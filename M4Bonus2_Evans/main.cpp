aeb5bd66630b981afefe1255a7b89a17476af872
evansm4141 <evansm4141@student.faytechcc.edu>
2022-03-28 10:53:32 -0400

    created menu

---
+++ M4Bonus2_Evans/main.cpp
@@ -0,0 +1,31 @@
+/* CSC 134
+M4BONUS2
+Melody Evans
+3-28-22 */
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Welcome to Sleeping Dogs II\n";
+    //declare vars
+    string gameStart;
+    //user input
+    cout << "Would you like to start a new game or quit game?\n";
+    cout << "Enter [Start] or [Quit]\n";
+    cin >> gameStart;
+    //branches
+    if (gameStart=="Start") {
+        cout << "Welcome to Hong Kong.\n\n";
+    }
+    else if (gameStart=="Quit") {
+        cout << "You have exited the game.\n";
+    }
+    while (gameStart!="Start" || gameStart!="Quit") {
+        cout << "Invalid input\n";
+        cout << "Enter [Start] or [Quit]\n";
+        cin >> gameStart;
+    }
+
+    return 0;
+}
